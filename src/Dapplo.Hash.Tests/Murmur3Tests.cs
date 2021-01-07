3ed7c9d80a4243d4de0734faa39d09f059dc596c
Robin Krom <robin@getgreenshot.org>
2021-01-08 00:16:42 +0100

    Improved the test with something with less than 4 bytes. [skip ci]

--- src/Dapplo.Hash.Tests/Murmur3Tests.cs
+++ src/Dapplo.Hash.Tests/Murmur3Tests.cs
@@ -14,23 +14,33 @@ namespace Dapplo.Hash.Tests
     {
         private static readonly uint Seed = 0x9747b28c;
         private static readonly string TestString = "The quick brown fox jumps over the lazy dog";
-        
+        private static readonly string ShortString = "S";
+
         [Fact]
         public void Murmur3_UTF8_Test()
         {
-            var murmur3Span = new Murmur3(Seed);
+            var murmur3Hasher = new Murmur3(Seed);
             var testBytes = Encoding.UTF8.GetBytes(TestString);
-            var hash = murmur3Span.CalculateHash(testBytes.AsSpan());
+            var hash = murmur3Hasher.CalculateHash(testBytes.AsSpan());
             Assert.Equal(0x2FA826CDu, hash);
         }
 
         [Fact]
         public void Murmur3_String_Test()
         {
-            var murmur3Span = new Murmur3(Seed);
+            var murmur3Hasher = new Murmur3(Seed);
 
-            var hash = murmur3Span.CalculateHash(TestString);
+            var hash = murmur3Hasher.CalculateHash(TestString);
             Assert.Equal((uint)96102032, hash);
+        }
+
+        [Fact]
+        public void Murmur3_Short_String_Test()
+        {
+            var murmur3Hasher = new Murmur3(Seed);
+
+            var hash = murmur3Hasher.CalculateHash(ShortString);
+            Assert.Equal((uint)3419752435, hash);
         }
     }
 }
